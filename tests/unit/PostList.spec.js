98dce5250e3260dd5845d5b1a89f63005d7e6f50
Abok Isaac <abokisaac@gmail.com>
2019-08-15 12:22:27 +0300

    Added tests for post list component

---
+++ tests/unit/PostList.spec.js
@@ -0,0 +1,54 @@
+import { shallowMount, createLocalVue } from '@vue/test-utils';
+import Vuex from 'vuex';
+import PostList from '@/components/PostList.vue';
+
+const localVue = createLocalVue();
+
+localVue.use(Vuex);
+
+const testData = [
+  {
+    id: '1',
+    title: 'Hi there!',
+    body: 'Another form of gretting',
+  },
+  {
+    id: '2',
+    title: 'Hi there!',
+    body: 'Another form of gretting',
+  },
+  {
+    id: '3',
+    title: 'Hi there!',
+    body: 'Another form of gretting',
+  },
+];
+
+describe('PostList.vue', () => {
+  let actions;
+  let store;
+  let getters;
+
+  beforeEach(() => {
+    actions = {
+      getPostsData: jest.fn(),
+    };
+    getters = {
+      getPosts: jest.fn(() => testData),
+    };
+    store = new Vuex.Store({ actions, getters });
+    shallowMount(PostList, { store, localVue });
+  });
+
+  it('has a created hook', () => {
+    expect(typeof PostList.created).toBe('function');
+    expect(actions.getPostsData).toHaveBeenCalled();
+  });
+
+  it('renders the list of posts', () => {
+    console.log(PostList.computed.getPosts());
+    expect(typeof PostList.computed.getPosts).toBe('function');
+    expect(getters.getPosts).toHaveBeenCalled();
+    expect(getters.getPosts().length).toBe(3);
+  });
+});
